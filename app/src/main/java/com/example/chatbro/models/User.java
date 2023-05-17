1d4152419e673761e04304ee728341bd864b86f6
saptarshisarkar555 <saptarshisarkar555@gmail.com>
2023-05-17 23:37:49 +0600

    First commit...

---
+++ app/src/main/java/com/example/chatbro/models/User.java
@@ -0,0 +1,7 @@
+package com.example.chatbro.models;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    public String name, image, email, token;
+}
---
+++ app/src/main/java/com/example/chatbro/utilities/Constants.java
@@ -0,0 +1,13 @@
+package com.example.chatbro.utilities;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_PREFERENCE_NAME = "chatBroPreference";
+    public static final String KEY_IS_SIGNED_IN = "isSignedIn";
+    public static final String KRY_USER_ID = "userId";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_FCM_TOKEN = "fcmToken";
+}
